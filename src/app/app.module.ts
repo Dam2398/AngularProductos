64213ed149224c188a5cacaf2423d41b4b58f4ed
Damian <ojuandamian@gmail.com>
2021-04-02 19:57:47 -0600

    cambio de curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';//modulo principal
 import { BrowserModule } from '@angular/platform-browser';
 //todos los componentes se deben declarar en app.module.ts (aqui)
 import { AppComponent } from './app.component';//importamos este componente
-import { MiComponente } from './components/mi-componente/mi-componente.component';
-import { PeliculasComponent } from './components/peliculas/peliculas.component';
+
 
 @NgModule({
   declarations: [//aqui se pueden declarar componentes, directivas, pipes
-    AppComponent,
-    MiComponente,
-    PeliculasComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
